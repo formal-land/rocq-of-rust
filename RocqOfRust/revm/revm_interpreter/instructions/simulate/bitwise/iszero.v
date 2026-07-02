bb178a66f554939e9c7eb8271861b501863ce5b6
cascade77 <ssiliconthingss@gmail.com>
2026-07-02 13:37:19 +0500

    Update Revm v103 bitwise iszero simulate slice

--- RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/iszero.v
+++ RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/iszero.v
@@ -2,10 +2,11 @@ Require Import simulate.RocqOfRust.
 Require Import core.links.array.
 Require Import core.links.cmp.
 Require Import core.simulate.cmp.
-Require Import revm.revm_context_interface.links.host.
+Require Import core.simulate.option.
 Require Import revm.revm_interpreter.gas.simulate.constants.
 Require Import revm.revm_interpreter.instructions.links.bitwise.iszero.
 Require Import revm.revm_interpreter.instructions.simulate.macros.
+Require Import revm.revm_interpreter.links.instruction_context.
 Require Import revm.revm_interpreter.links.interpreter.
 Require Import revm.revm_interpreter.links.interpreter_types.
 Require Import revm.revm_interpreter.simulate.interpreter_types.
@@ -19,7 +20,6 @@ Definition op_iszero
     {IInterpreterTypes : InterpreterTypes.C WIRE_types}
     (interpreter : Interpreter.t WIRE WIRE_types) :
     Interpreter.t WIRE WIRE_types :=
-  gas_macro interpreter constants.VERYLOW id (fun interpreter =>
   popn_top_macro interpreter {| Integer.value := 0 |} id (fun arr top interpreter =>
     let op1 := top.(RefStub.projection) interpreter.(Interpreter.stack) in
     let result :=
@@ -32,12 +32,11 @@ Definition op_iszero
         interpreter.(Interpreter.stack) result in
     interpreter
       <| Interpreter.stack := stack |>
-  )).
+  ).
 
 Lemma op_iszero_eq
     {WIRE H : Set} `{Link WIRE} `{Link H}
     {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
-    {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
     (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
     (IInterpreterTypes : InterpreterTypes.C WIRE_types)
     (InterpreterTypesEq :
@@ -46,9 +45,13 @@ Lemma op_iszero_eq
     (_host : H) :
   let ref_interpreter : '&mut (Interpreter.t WIRE WIRE_types) := make_ref 0 in
   let ref_host : '&mut H := make_ref 1 in
+  let context := {|
+    instruction_context.InstructionContext.interpreter := ref_interpreter;
+    instruction_context.InstructionContext.host := ref_host;
+  |} in
   {{
     SimulateM.eval_f
-      (run_bitwise_is_zero run_InterpreterTypes_for_WIRE ref_interpreter ref_host)
+      (run_bitwise_is_zero run_InterpreterTypes_for_WIRE context)
       ([interpreter; _host]%stack) 🌲
     (
       Output.Success tt,
@@ -60,9 +63,13 @@ Lemma op_iszero_eq
   }}.
 Proof.
   intros.
-  unfold op_iszero.
-  gas_macro_eq idtac.
+  with_strategy transparent [run_bitwise_is_zero] unfold op_iszero, run_bitwise_is_zero; cbn.
   popn_top_macro_eq InterpreterTypesEq.
+  eapply Run.Call; [
+    eapply Impl_Option.unwrap_unchecked_eq;
+    reflexivity
+  |].
+  cbn.
   lu.
   c. { apply Impl_Uint.is_zero_eq; repeat unshelve econstructor. }
   c. { apply Impl_Uint.from_eq; [typeclasses eauto | easy]. }
