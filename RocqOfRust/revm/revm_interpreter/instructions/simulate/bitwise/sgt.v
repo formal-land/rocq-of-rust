06a1924fbf6a68a614686af4c2ef4b681f5362a8
cascade77 <ssiliconthingss@gmail.com>
2026-07-02 15:31:27 +0500

    Update sgt link and simulate to v103
    Update sgt to use InstructionContext and match the v103 body without static gas in simulate.

--- RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/sgt.v
+++ RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/sgt.v
@@ -3,11 +3,12 @@ Require Import alloy_primitives.links.aliases.
 Require Import core.links.array.
 Require Import core.links.cmp.
 Require Import core.simulate.cmp.
-Require Import revm.revm_context_interface.links.host.
+Require Import core.simulate.option.
 Require Import revm.revm_interpreter.gas.simulate.constants.
 Require Import revm.revm_interpreter.instructions.links.bitwise.sgt.
 Require Import revm.revm_interpreter.instructions.simulate.i256.
 Require Import revm.revm_interpreter.instructions.simulate.macros.
+Require Import revm.revm_interpreter.links.instruction_context.
 Require Import revm.revm_interpreter.links.interpreter.
 Require Import revm.revm_interpreter.links.interpreter_types.
 Require Import revm.revm_interpreter.simulate.interpreter_types.
@@ -21,26 +22,24 @@ Definition op_sgt
     {IInterpreterTypes : InterpreterTypes.C WIRE_types}
     (interpreter : Interpreter.t WIRE WIRE_types) :
     Interpreter.t WIRE WIRE_types :=
-  gas_macro interpreter constants.VERYLOW id (fun interpreter =>
   popn_top_macro interpreter {| Integer.value := 1 |} id (fun arr top interpreter =>
-  let '⟬ op1 ⟭ := arr.(array.value) in
-  let op2 := top.(RefStub.projection) interpreter.(Interpreter.stack) in
-  let result :=
-    match i256_cmp op1 op2 with
-    | Ordering.Greater => {| Uint.value := 1 |}
-    | _ => {| Uint.value := 0 |}
-    end in
-  let stack :=
-    top.(RefStub.injection)
-      interpreter.(Interpreter.stack) result in
-  interpreter
-    <| Interpreter.stack := stack |>
-  )).
+    let '⟬ op1 ⟭ := arr.(array.value) in
+    let op2 := top.(RefStub.projection) interpreter.(Interpreter.stack) in
+    let result :=
+      match i256_cmp op1 op2 with
+      | Ordering.Greater => {| Uint.value := 1 |}
+      | _ => {| Uint.value := 0 |}
+      end in
+    let stack :=
+      top.(RefStub.injection)
+        interpreter.(Interpreter.stack) result in
+    interpreter
+      <| Interpreter.stack := stack |>
+  ).
 
 Lemma op_sgt_eq
     {WIRE H : Set} `{Link WIRE} `{Link H}
     {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
-    {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
     (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
     (IInterpreterTypes : InterpreterTypes.C WIRE_types)
     (InterpreterTypesEq :
@@ -49,9 +48,13 @@ Lemma op_sgt_eq
     (_host : H) :
   let ref_interpreter : '&mut (Interpreter.t WIRE WIRE_types) := make_ref 0 in
   let ref_host : '&mut H := make_ref 1 in
+  let context := {|
+    instruction_context.InstructionContext.interpreter := ref_interpreter;
+    instruction_context.InstructionContext.host := ref_host;
+  |} in
   {{
     SimulateM.eval_f
-      (run_sgt run_InterpreterTypes_for_WIRE ref_interpreter ref_host)
+      (run_sgt run_InterpreterTypes_for_WIRE context)
       ([interpreter; _host]%stack) 🌲
     (
       Output.Success tt,
@@ -63,9 +66,13 @@ Lemma op_sgt_eq
   }}.
 Proof.
   intros.
-  unfold op_sgt.
-  gas_macro_eq idtac.
+  with_strategy transparent [run_sgt] unfold op_sgt, run_sgt; cbn.
   popn_top_macro_eq InterpreterTypesEq.
+  eapply Run.Call; [
+    eapply Impl_Option.unwrap_unchecked_eq;
+    reflexivity
+  |].
+  cbn.
   match goal with
   | array : array.t aliases.U256.t _ |- _ =>
     destruct array as [[op1 []]]
