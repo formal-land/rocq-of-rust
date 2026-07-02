509e613c247310302632cd91346660ed7444d416
Arooj <ssiliconthingss@gmail.com>
2026-07-02 17:28:36 +0500

    Update not link and simulate to v103 (#872)
    Update not link and simulate to the v103 InstructionContext shape.

--- RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/not.v
+++ RocqOfRust/revm/revm_interpreter/instructions/simulate/bitwise/not.v
@@ -3,10 +3,11 @@ Require Import core.links.array.
 Require Import core.links.cmp.
 Require Import core.ops.simulate.bit.
 Require Import core.simulate.cmp.
-Require Import revm.revm_context_interface.links.host.
+Require Import core.simulate.option.
 Require Import revm.revm_interpreter.gas.simulate.constants.
 Require Import revm.revm_interpreter.instructions.links.bitwise.not.
 Require Import revm.revm_interpreter.instructions.simulate.macros.
+Require Import revm.revm_interpreter.links.instruction_context.
 Require Import revm.revm_interpreter.links.interpreter.
 Require Import revm.revm_interpreter.links.interpreter_types.
 Require Import revm.revm_interpreter.simulate.interpreter_types.
@@ -21,7 +22,6 @@ Definition op_not
     {IInterpreterTypes : InterpreterTypes.C WIRE_types}
     (interpreter : Interpreter.t WIRE WIRE_types) :
     Interpreter.t WIRE WIRE_types :=
-  gas_macro interpreter constants.VERYLOW id (fun interpreter =>
   popn_top_macro interpreter {| Integer.value := 0 |} id (fun arr top interpreter =>
     let op1 := top.(RefStub.projection) interpreter.(Interpreter.stack) in
     let result := Impl_Not_for_Uint.not op1 in
@@ -30,12 +30,11 @@ Definition op_not
         interpreter.(Interpreter.stack) result in
     interpreter
       <| Interpreter.stack := stack |>
-  )).
+  ).
 
 Lemma op_not_eq
     {WIRE H : Set} `{Link WIRE} `{Link H}
     {WIRE_types : InterpreterTypes.Types.t} `{InterpreterTypes.Types.AreLinks WIRE_types}
-    {H_types : Host.Types.t} `{Host.Types.AreLinks H_types}
     (run_InterpreterTypes_for_WIRE : InterpreterTypes.Run WIRE WIRE_types)
     (IInterpreterTypes : InterpreterTypes.C WIRE_types)
     (InterpreterTypesEq :
@@ -44,9 +43,13 @@ Lemma op_not_eq
     (_host : H) :
   let ref_interpreter : '&mut (Interpreter.t WIRE WIRE_types) := make_ref 0 in
   let ref_host : '&mut H := make_ref 1 in
+  let context := {|
+    instruction_context.InstructionContext.interpreter := ref_interpreter;
+    instruction_context.InstructionContext.host := ref_host;
+  |} in
   {{
     SimulateM.eval_f
-      (run_bitwise_not run_InterpreterTypes_for_WIRE ref_interpreter ref_host)
+      (run_bitwise_not run_InterpreterTypes_for_WIRE context)
       ([interpreter; _host]%stack) 🌲
     (
       Output.Success tt,
@@ -58,9 +61,13 @@ Lemma op_not_eq
   }}.
 Proof.
   intros.
-  unfold op_not.
-  gas_macro_eq idtac.
+  with_strategy transparent [run_bitwise_not] unfold op_not, run_bitwise_not; cbn.
   popn_top_macro_eq InterpreterTypesEq.
+  eapply Run.Call; [
+    eapply Impl_Option.unwrap_unchecked_eq;
+    reflexivity
+  |].
+  cbn.
   lu.
   cw @Not.Eq.not.
   pf.
