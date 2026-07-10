cd09e5dfcd50b4cc79030123c14901a210eb2378
Arooj <ssiliconthingss@gmail.com>
2026-07-10 14:04:34 +0500

    Use links plugin for generic ruint from type (#923)
    Adds generic enum support to the links plugin and uses it to migrate ruint `ToUintError<T>` away from its jinja template.

--- RocqOfRust/plugin/test_inline_print.v
+++ RocqOfRust/plugin/test_inline_print.v
@@ -14,3 +14,17 @@ Print DebugOrdering.IsOfTy.
 Print DebugOrdering.IsOfValueWith_Less.
 Print DebugOrdering.IsOfValue_Less.
 Print DebugOrdering.SubPointer.
+
+Module DebugGeneric.
+  RocqOfRustLinkGenericEnum "debug::Generic" [ T ] :=
+  | WithValue (value : T)
+  | Empty
+  .
+End DebugGeneric.
+
+Print DebugGeneric.t.
+Print DebugGeneric.IsLink.
+Print DebugGeneric.IsOfTy.
+Check DebugGeneric.IsOfValueWith_WithValue.
+Check DebugGeneric.IsOfValue_WithValue.
+Check DebugGeneric.SubPointer.get_WithValue_0.
